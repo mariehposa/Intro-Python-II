7039e537d00116cd7a0ce589d9c048db0b9cccb8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-29 15:57:48 +0100

    wip

--- src/player.py
+++ src/player.py
@@ -1,6 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, room):
+    def __init__(self, name, room, item):
         self.name = name
         self.room = room
+        self.item = item
+
+    def __str__(self):
+        items = []
+        items.append(self.item)
+        return items
